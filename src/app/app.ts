f599ffd8e9a0169347c41c8062a5b9bd70f4de85
Camila Forero <amandaforero@outlook.com>
2025-08-05 17:13:22 -0500

    Creacion base de proyecto

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('todoAppPrueba');
+  saludo = 'Holaaaa este es mi primer proyecto';
 }
